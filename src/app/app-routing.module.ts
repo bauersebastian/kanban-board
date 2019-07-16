d633385c3f818e51dafce8a59aabc16c8482cfb7
sebastian <bauerseb@icloud.com>
2019-07-16 23:39:21 +0200

    basic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {KanbanBoardComponent} from './kanban-board/kanban-board.component';
+import {ColumnDetailsComponent} from './column-details/column-details.component';
+import {ModuleDetailsComponent} from './module-details/module-details.component';
+import {TaskDetailsComponent} from './task-details/task-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/kanban-board', pathMatch: 'full' },
+  { path: 'kanban-board', component: KanbanBoardComponent},
+  { path: 'column-add', component: ColumnDetailsComponent},
+  { path: 'module-add', component: ModuleDetailsComponent},
+  { path: 'task-add', component: TaskDetailsComponent},
+  { path: 'task/:id', component: TaskDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
+import { ColumnDetailsComponent } from './column-details/column-details.component';
+import { ModuleDetailsComponent } from './module-details/module-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule, MatIconModule, MatButtonModule, MatGridListModule, MatDividerModule, MatCardModule } from '@angular/material';
+import { ModuleRowComponent } from './module-row/module-row.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KanbanBoardComponent,
+    TaskDetailsComponent,
+    ColumnDetailsComponent,
+    ModuleDetailsComponent,
+    ModuleRowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
